c03264bf0791582629b52c580fc42da8131380a5
Sean Ray <sean.ray@wwt.com>
2019-02-28 08:17:39 -0500

    Simple get/add of books
    * Add `/my-books` get and post endpoints
    * Save to a (local) dynamodb instance
    
    Other changes
    
    * Handle errors when parsing xml from goodreads API

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
-
+const AWS = require("aws-sdk");
 const express = require("express");
 const cors = require("cors");
 const asyncHandler = require("express-async-handler");
 const axios = require("axios");
 const xml2js = require("xml2js");
+const uuidv4 = require("uuid/v4");
+let bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3001;
 
+AWS.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+app.use(bodyParser.json());
 app.use(
   cors({
     origin: [
@@ -20,6 +30,50 @@ app.use(
 );
 
 app.get("/", (req, res) => res.send({ hello: "world" }));
+
+app.get(
+  "/my-books",
+  asyncHandler(async (req, res, next) => {
+    docClient.scan({ TableName: "mybooks" }, function(err, data) {
+      if (err) {
+        return next(err);
+      } else {
+        res.json(data.Items);
+      }
+    });
+  })
+);
+
+app.post(
+  "/my-books",
+  asyncHandler(async (req, res, next) => {
+    let book = {
+      TableName: "mybooks",
+      Item: {
+        id: uuidv4(),
+        title: req.body.title
+      }
+    };
+    docClient.put(book, function(err) {
+      if (err) {
+        return next(err);
+      } else {
+        var params = {
+          TableName: "mybooks",
+          Key: {
+            id: book.Item.id
+          }
+        };
+        docClient.get(params, function(err, data) {
+          if (err) {
+            return next(err);
+          }
+          res.status(201).json(data.Item);
+        });
+      }
+    });
+  })
+);
 
 app.get(
   "/books",
@@ -45,11 +99,24 @@ app.get(
         valueProcessors: [xml2js.processors.parseNumbers]
       },
       function(err, result) {
+        if (err) {
+          throw err;
+        }
+        res.set("Cache-Control", "private, max-age=86400");
         res.json(result.GoodreadsResponse.search);
       }
     );
   })
 );
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  // eslint-disable-next-line no-console
+  console.error(err.message || err, err.stack);
+  res.status(500).json({ message: err.message || err.toString() });
+});
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`App started at http://localhost:${port}`));
